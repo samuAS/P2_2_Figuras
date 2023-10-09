ec235598cc8ab1a67ebfe5de0e7b775ab58f33eb
Samuel <samee90as@gmail.com>
2023-10-09 10:17:53 +0200

    Second Commit

--- P2_2_Figuras.cs
+++ P2_2_Figuras.cs
@@ -1,6 +1,7 @@
 ﻿//necesario para usar la estructura Color
 // https://learn.microsoft.com/es-es/dotnet/api/system.drawing.color?view=net-7.0
 using System.Drawing;
+using _P2_2_Figuras;
 //método para obtener un color a partir de los valores RGBA 
 // https://learn.microsoft.com/es-es/dotnet/api/system.drawing.color.fromargb?view=net-7.0
 
@@ -10,7 +11,16 @@ public class P2_2_Figuras
 {
   static void Main(string[] args)
   {
+    Rectangulo rectangulo = new Rectangulo(Color.FromArgb(255, 50, 20, 34), 3, 3.9);
+    Circulo circulo1 = new Circulo(Color.FromArgb(255, 0, 20, 35), 4);
+    Circulo circulo2 = new Circulo(Color.FromArgb(255, 230, 20, 30), 2);
+    TrianguloEquilatero triangulo = new TrianguloEquilatero(Color.FromArgb(255, 0, 20, 40), 3);
 
+    Console.WriteLine(rectangulo);
+    Console.WriteLine(circulo1);
+    Console.WriteLine(circulo2);
+    Console.WriteLine(triangulo);
   }
+
 
 }
